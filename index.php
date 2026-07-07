e227408c2721e4fad479f3acb6c7b756063fba37
kihbdedefhihde <kihbdedefhihde@gmail.com>
2026-07-07 19:32:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,20 @@ TemplateMo 613 Frost Bakery
 https://templatemo.com/tm-613-frost-bakery
 
 -->
+
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a4c138ab225131d45678281/1jssiolqi';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </head>
 <body>
 
